941085b1ed79ec33ac4f45854521fd16b254a69e
Matthew Evers <mreverssci@gmail.com>
2020-10-08 19:04:13 -0500

    feat: no lazy load

--- projects/client/vanilla-client/src/app/routes.tsx
+++ projects/client/vanilla-client/src/app/routes.tsx
@@ -3,28 +3,9 @@ import { Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route, useParams } from 'react-router-dom';
 import { LoadingIndicator } from '../components/LoadingIndicator';
 
-const UsersList = lazy(() => import(/* webpackChunkName: "users-list" */ '../views/UsersList'));
-const UserProfile = lazy(() => import(/* webpackChunkName: "user-profile" */ '../views/UserProfile'));
-const UserTag = lazy(() => import(/* webpackChunkName: "user-tag" */ '../views/UserTag'));
-
-function LoadingSuspense(props: React.PropsWithChildren<{}>) {
-	const { children } = props;
-	return (
-		<Suspense
-			fallback={
-				<LoadingIndicator
-					style={{
-						margin: 'auto',
-						height: '32px',
-						width: '32px'
-					}}
-				/>
-			}
-		>
-			{children}
-		</Suspense>
-	);
-}
+import UsersList from '../views/UsersList';
+import UserProfile from '../views/UserProfile';
+import UserTag from '../views/UserTag';
 
 function WrappedUserProfile() {
 	const { id } = useParams() as any;
@@ -41,19 +22,17 @@ function WrappedUserTag() {
 export function Routes() {
 	return (
 		<BrowserRouter>
-			<LoadingSuspense>
-				<Switch>
-					<Route path="/" exact>
-						<UsersList />
-					</Route>
-					<Route path="/user/:id" exact>
-						<WrappedUserProfile />
-					</Route>
-					<Route path="/user/:id/tags/:tagId">
-						<WrappedUserTag />
-					</Route>
-				</Switch>
-			</LoadingSuspense>
+			<Switch>
+				<Route path="/" exact>
+					<UsersList />
+				</Route>
+				<Route path="/user/:id" exact>
+					<WrappedUserProfile />
+				</Route>
+				<Route path="/user/:id/tags/:tagId">
+					<WrappedUserTag />
+				</Route>
+			</Switch>
 		</BrowserRouter>
 	);
 }
